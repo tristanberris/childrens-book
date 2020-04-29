992d961dcacbe881f7081cace8d0fa2c5a92c966
RickCodeworks <rjedgemon@hotmail.com>
2020-04-29 11:16:29 -0600

    small change

--- clientsrc/src/components/navbar.vue
+++ clientsrc/src/components/navbar.vue
@@ -1,42 +1,21 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <router-link class="navbar-brand" :to="{ name: 'Home' }"
-      >AppName</router-link
-    >
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarText"
-      aria-controls="navbarText"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
+    <router-link class="navbar-brand" :to="{ name: 'Home' }">AppName</router-link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
+      aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item" :class="{ active: $route.name == 'Home' }">
-          <router-link :to="{ name: 'Home' }" class="nav-link"
-            >Home</router-link
-          >
+          <router-link :to="{ name: 'Home' }" class="nav-link">Home</router-link>
         </li>
-        <li
-          class="nav-item"
-          v-if="$auth.isAuthenticated"
-          :class="{ active: $route.name == 'Profile' }"
-        >
-          <router-link class="nav-link" :to="{ name: 'Profile' }"
-            >Profile</router-link
-          >
+        <li class="nav-item" v-if="$auth.isAuthenticated" :class="{ active: $route.name == 'Profile' }">
+          <router-link class="nav-link" :to="{ name: 'Profile' }">My Profile</router-link>
         </li>
       </ul>
       <span class="navbar-text">
-        <button
-          class="btn btn-success"
-          @click="login"
-          v-if="!$auth.isAuthenticated"
-        >
+        <button class="btn btn-success" @click="login" v-if="!$auth.isAuthenticated">
           Login
         </button>
         <button class="btn btn-danger" @click="logout" v-else>logout</button>
@@ -46,24 +25,24 @@
 </template>
 
 <script>
-import axios from "axios";
-import { getUserData } from "@bcwdev/auth0-vue";
-export default {
-  name: "Navbar",
-  methods: {
-    async login() {
-      await this.$auth.loginWithPopup();
-      this.$store.dispatch("setBearer", this.$auth.bearer);
-      console.log("this.$auth.user: ");
-      console.log(this.$auth.user);
-      this.$store.dispatch("getProfile");
-    },
-    async logout() {
-      this.$store.dispatch("resetBearer");
-      await this.$auth.logout({returnTo: window.location.origin});
+  import axios from "axios";
+  import { getUserData } from "@bcwdev/auth0-vue";
+  export default {
+    name: "Navbar",
+    methods: {
+      async login() {
+        await this.$auth.loginWithPopup();
+        this.$store.dispatch("setBearer", this.$auth.bearer);
+        console.log("this.$auth.user: ");
+        console.log(this.$auth.user);
+        this.$store.dispatch("getProfile");
+      },
+      async logout() {
+        this.$store.dispatch("resetBearer");
+        await this.$auth.logout({ returnTo: window.location.origin });
+      }
     }
-  }
-};
+  };
 </script>
 
 <style></style>
