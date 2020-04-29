46ec96cd33f24c795d133653454ad6322763f622
Eva Rozsa <Eva Rozsa>
2020-04-29 16:17:59 -0600

    Home page book layout

--- clientsrc/src/Pages/Home.vue
+++ clientsrc/src/Pages/Home.vue
@@ -1,7 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <h1>Welcome to Your Vue.js App</h1>
+  <div class="home container-fluid">
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <h1 class="border">Welcome to Your Vue.js App</h1>
+    <div class="text-center">
+      <!-- this button sends you to the create page, maybe router link? -->
+      <button class="btn btn-lg btn-info px-5 border">Create A Book</button>
+    </div>
+    <div class="row border m-1 pl-5">
+      <!-- this is were we would import books, they need to be col-3 -->
+      <!-- when you click on a posted book, large modal to read book. -->
+      <div class="col-md-3">
+        <img
+          class="madeBooks"
+          src="https://cdn3.vectorstock.com/i/1000x1000/62/02/kids-boy-reading-book-vector-16706202.jpg"
+          alt
+        />
+      </div>
+      <div class="col-md-3">
+        <img
+          class="madeBooks"
+          src="https://cdn3.vectorstock.com/i/1000x1000/62/02/kids-boy-reading-book-vector-16706202.jpg"
+          alt
+        />
+      </div>
+      <div class="col-md-3">
+        <img
+          class="madeBooks"
+          src="https://cdn3.vectorstock.com/i/1000x1000/62/02/kids-boy-reading-book-vector-16706202.jpg"
+          alt
+        />
+      </div>
+      <div class="col-md-3">
+        <img
+          class="madeBooks"
+          src="https://cdn3.vectorstock.com/i/1000x1000/62/02/kids-boy-reading-book-vector-16706202.jpg"
+          alt
+        />
+      </div>
+      <div class="col-md-3">
+        <img
+          class="madeBooks"
+          src="https://cdn3.vectorstock.com/i/1000x1000/62/02/kids-boy-reading-book-vector-16706202.jpg"
+          alt
+        />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,3 +53,12 @@ export default {
   name: "home"
 };
 </script>
+<style>
+.madeBooks {
+  height: 50vh;
+  max-width: 60vw;
+  margin-top: 30px;
+  box-shadow: 2px 2px 5px black;
+  padding: 10px;
+}
+</style>
