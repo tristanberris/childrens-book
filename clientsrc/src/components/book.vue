34d23215be2618af662d61f1ad8ae9fe9984b7f4
light-attracts-bugs <micahurizar@gmail.com>
2020-05-01 10:56:52 -0600

    got modal started

--- clientsrc/src/components/book.vue
+++ clientsrc/src/components/book.vue
@@ -1,14 +1,49 @@
 <template>
-  <div class="col-md-5 col-sm-12 col-lg-3 col-xl-2 m-1 madeBooks img d-flex flex-column " :style="{backgroundImage:`url(${bookData.coverImg})`}">
+  <a href="" data-toggle="modal" data-target="#exampleModalCenter">
+  <div
+    class="col-md-5 col-sm-12 col-lg-3 col-xl-2 m-1 madeBooks img d-flex flex-column"
+    :style="{backgroundImage:`url(${bookData.coverImg})`}"
+  >
     <!-- <img v-bind:src="bookData.coverImg" class="img img-fluid bg"  alt=""/> -->
 
-   
-    <h2 class="align-self-start text-center w-100 mb-auto">
-        {{bookData.title}}
-        </h2>
-    
-    <h4 class="text-center  mx-auto">{{bookData.author}}</h4>
+    <h2 class="align-self-start text-center w-100 mb-auto">{{bookData.title}}</h2>
+
+    <h4 class="text-center mx-auto">{{bookData.author}}</h4>
+    <!-- Button trigger modal -->
+    <!-- <button
+      type="button"
+      class="btn btn-primary"
+      data-toggle="modal"
+      data-target="#exampleModalCenter"
+    >Launch demo modal</button> -->
+
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      id="exampleModalCenter"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalCenterTitle"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">...</div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+    </a>
 </template>
 
 
@@ -55,6 +90,4 @@ export default {
   min-width: 20vw; */
   /* padding-top: 15%; */
 }
-
-
 </style>
