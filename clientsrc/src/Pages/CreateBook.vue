55916730e7cb1f10a1ef1e96537878a5abd4db57
DennisMWarner <warner.dennis.m@gmail.com>
2020-05-04 13:31:52 -0600

    stubs out saving and transforms

--- clientsrc/src/Pages/CreateBook.vue
+++ clientsrc/src/Pages/CreateBook.vue
@@ -1,66 +1,31 @@
 <template>
   <div class="CreateBook container-fluid">
     <div class="row mt-2 mb-5">
-      <div
-        class="col-md-8 bg-light border rounded border-dark"
-        width="410"
-        height="500"
-      >
+      <div class="col-md-8 bg-light border rounded border-dark" width="410" height="500">
+        <button class="btn" @click="save()">Save</button>
+        <button class="btn" @click="picture()">Picture</button>
+
         <v-stage
           ref="stage"
+          class="main-stage"
           :config="configKonva"
-          @dragstart="handleDragstart"
-          @dragend="handleDragend"
-          id="container"
+          @drop="drop"
+          @dragover.prevent
+          @mousedown="handleStageMouseDown"
+          dropzone="page"
         >
-          <!-- <v-layer ref="layer">
-          <v-star v-for="item in list" :key="item.id" :config="{
-                      x: item.x,
-                      y: item.y,
-                      rotation: item.rotation,
-                      id: item.id,
-                      numPoints: 5,
-                      innerRadius: 30,
-                      outerRadius: 50, fill: '#89b717',
-                      opacity: 0.8,
-                      draggable: true,
-                      scaleX: dragItemId === item.id ? item.scale * 1.2 : item.scale,
-                      scaleY: dragItemId === item.id ? item.scale * 1.2 : item.scale,
-                      shadowColor: 'black',
-                      shadowBlur: 10,
-                      shadowOffsetX: dragItemId === item.id ? 15 : 5,
-                      shadowOffsetY: dragItemId === item.id ? 15 : 5,
-                      shadowOpacity: 0.6
-                    }"></v-star>
-        </v-layer> -->
           <v-layer ref="layer">
             <v-image
-              :config="{
-                image: image,
-                draggable: true,
-              }"
+              :id="img.id"
+              v-for="img in pageImages"
+              :config="img"
+              @dragend="(dragEvent) => handleMove(dragEvent,img)"
+              @transformend="(transformEvent) => handleTransformEnd(transformEvent, img)"
+              :key="img.id"
             />
+            <v-transformer ref="transformer" />
           </v-layer>
-          <!-- <v-layer ref="layer">
-          <v-image
-            :config="{
-              image: image,
-              draggable: true,
-            }"
-          />
-        </v-layer> -->
         </v-stage>
-        <!-- <v-stage
-        ref="stage"
-        :config="configKonva"
-        @dragstart="handleDragstart"
-        @dragend="handleDragend"
-      >
-        <v-layer ref="layer"> </v-layer>
-      </v-stage> -->
-        <!-- <div class="col-md-4 border selectBar" width="410" height="500">
-        some moveable text
-      </div> -->
       </div>
       <div
         class="col-md-4 border border-dark rounded shadow bg-light w-100 p-3"
@@ -75,28 +40,13 @@
           <div class="col-12">
             <div class="row">
               <!-- Images will be inserted here from a component -->
-              <div class="col-4">
+              <div class="col-4" v-for="img in imgOptions" :key="img.url">
                 <img
-                  src="../assets/Oldman.png"
-                  alt=""
+                  :src="img.url"
+                  alt
                   draggable="true"
                   class="img-fluid"
-                />
-              </div>
-              <div class="col-4">
-                <img
-                  src="../assets/Oldman.png"
-                  alt=""
-                  draggable="true"
-                  class="img-fluid"
-                />
-              </div>
-              <div class="col-4">
-                <img
-                  src="../assets/Oldman.png"
-                  alt=""
-                  draggable="true"
-                  class="img-fluid"
+                  @dragstart="dragStart(img)"
                 />
               </div>
             </div>
@@ -110,93 +60,159 @@
 <script>
 const width = window.innerWidth;
 const height = window.innerHeight;
+let stage = {};
 export default {
   data() {
     return {
-      list: [],
-      dragItemId: null,
+      imgOptions: [
+        { url: "/img/lady1.png" },
+        { url: "/img/Oldman.png" },
+        { url: "/img/oldman2.png" }
+      ],
       configKonva: {
         width: width,
-        height: height,
+        height: height
       },
-      image: null,
+      draggingItem: {},
+      selectedShapeId: ""
     };
   },
-  methods: {
-    handleDragstart(e) {
-      // save drag element:
-      this.dragItemId = e.target.id();
-      // move current element to the top:
-      const item = this.list.find((i) => i.id === this.dragItemId);
-      const index = this.list.indexOf(item);
-      this.list.splice(index, 1);
-      this.list.push(item);
+  computed: {
+    activePage() {
+      return this.$store.state.activePage;
     },
-    handleDragend(e) {
-      this.dragItemId = null;
-    },
-  },
-  mounted() {
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-
-    var stage = new Konva.Stage({
-      container: "container",
-      width: width * 0.65,
-      height: height,
-    });
-    var layer = new Konva.Layer();
-    stage.add(layer);
-
-    // what is url of dragging element?
-    var itemURL = "";
-    document
-      .getElementById("drag-items")
-      .addEventListener("dragstart", function(e) {
-        itemURL = e.target.src;
-      });
-
-    var con = stage.container();
-    con.addEventListener("dragover", function(e) {
-      e.preventDefault(); // !important
-    });
-
-    con.addEventListener("drop", function(e) {
-      e.preventDefault();
-      // now we need to find pointer position
-      // we can't use stage.getPointerPosition() here, because that event
-      // is not registered by Konva.Stage
-      // we can register it manually:
-      stage.setPointersPositions(e);
-
-      Konva.Image.fromURL(itemURL, function(image) {
-        layer.add(image);
-
-        image.position(stage.getPointerPosition());
-        image.draggable(true);
-
-        layer.draw();
-      });
-    });
-    for (let n = 0; n < 30; n++) {
-      this.list.push({
-        id: Math.round(Math.random() * 10000).toString(),
-        x: Math.random() * width,
-        y: Math.random() * height,
-        rotation: Math.random() * 180,
-        scale: Math.random(),
+    pageImages() {
+      let vm = this;
+      return this.activePage.images.map(imgData => {
+        return {
+          ...imgData,
+          image: vm.createImageElem(imgData.url)
+        };
       });
     }
   },
-  created() {
-    const image = new window.Image();
-    image.src =
-      "http://www.clker.com/cliparts/4/6/b/a/11949845361959461363man_01.svg.med.png";
-    image.onload = () => {
-      // set image only when it is loaded
-      this.image = image;
-    };
+  methods: {
+    save() {
+      // save the active page
+    },
+    load() {},
+    picture() {
+      stage.toImage({
+        callback: img => {
+          console.log(img.src);
+        }
+      });
+    },
+    dragStart(img) {
+      this.draggingItem = img;
+      // // save drag element:
+      // this.dragItemId = e.target.id();
+      // // move current element to the top:
+      // const item = this.list.find(i => i.id === this.dragItemId);
+      // const index = this.list.indexOf(item);
+      // this.list.splice(index, 1);
+      // this.list.push(item);
+    },
+    drop(e) {
+      // TODO take the image beign dragged and add it ot the active page
+      // activePage.images
+    },
+
+    // DONT TOUCH
+    handleMove(konvaDragEvent, img) {
+      try {
+        img.x = konvaDragEvent.target.attrs.x;
+        img.y = konvaDragEvent.target.attrs.y;
+      } catch (e) {
+        console.error(e);
+      }
+    },
+    handleTransformEnd(konvaTransformEvent, img) {
+      // update the state
+      img.x = konvaTransformEvent.target.x();
+      img.y = konvaTransformEvent.target.y();
+      img.rotation = konvaTransformEvent.target.rotation();
+      img.scaleX = konvaTransformEvent.target.scaleX();
+      img.scaleY = konvaTransformEvent.target.scaleY();
+    },
+    createImageElem(url) {
+      let image = new window.Image();
+      image.src = url;
+      return image;
+    },
+    handleStageMouseDown(e) {
+      this.selectedShapeId = "";
+      // clicked on stage - clear selection
+
+      let isMainStage = e.target.attrs.container
+        ? e.target.attrs.container.className.includes("main-stage")
+        : false;
+      if (isMainStage) {
+        this.selectedShapeId = "";
+        this.detachTransformer();
+        return;
+      }
+
+      // clicked on transformer - do nothing
+      const clickedOnTransformer =
+        e.target.getParent().className === "Transformer";
+      if (clickedOnTransformer) {
+        return;
+      }
+
+      // find clicked rect by its name
+      const id = e.target.attrs.id;
+      const imgData = this.pageImages.find(r => r.id === id);
+      if (imgData) {
+        this.selectedShapeId = id;
+      } else {
+        this.selectedShapeId = "";
+      }
+      this.updateTransformer();
+    },
+    updateTransformer() {
+      // here we need to manually attach or detach Transformer node
+      const transformerNode = this.$refs.transformer.getNode();
+      const stage = transformerNode.getStage();
+      const { selectedShapeId } = this;
+      const selectedNode = stage.findOne("#" + selectedShapeId);
+      // do nothing if selected node is already attached
+      if (selectedNode === transformerNode.node()) {
+        return;
+      }
+
+      if (selectedNode) {
+        // attach to another node
+        transformerNode.attachTo(selectedNode);
+      } else {
+        // remove transformer
+        transformerNode.detach();
+      }
+      transformerNode.getLayer().batchDraw();
+    },
+    detachTransformer() {
+      const transformerNode = this.$refs.transformer.getNode();
+      transformerNode.detach();
+      transformerNode.getLayer().batchDraw();
+    }
   },
+  mounted() {
+    window.TESTSTAGE = this.$refs.stage;
+    stage = this.$refs.stage.getStage();
+    setTimeout(() => {
+      console.log("drawing ");
+      stage.draw();
+    }, 300);
+  },
+  watch: {
+    activePage: {
+      deep: true,
+      // immediate: true,
+      handler() {
+        stage.draw();
+      }
+    }
+  }
 };
 </script>
 
